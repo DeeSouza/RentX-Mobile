b7eff311df75f7dc8662f1d77f647e8dd27fff98
Diego Souza <dee-souza@hotmail.com>
2020-10-26 12:44:43 -0300

    :construction: Working page profile progress

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
-import { Text } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container } from './styles';
+import ProfileImage from '../../assets/cars/profile.png';
+
+import {
+  Container,
+  Header,
+  HeaderWrapper,
+  TitleText,
+  ButtonEditProfile,
+  ButtonLogout,
+  InfoProfile,
+  ProfilePicture,
+  ImageProfile,
+} from './styles';
 
 const Profile: React.FC = () => {
   return (
     <Container>
-      <Text>Profile</Text>
+      <Header>
+        <HeaderWrapper>
+          <ButtonEditProfile onPress={() => {}}>
+            <Icon name="edit-3" size={24} color="#AEAEB3" />
+          </ButtonEditProfile>
+          <TitleText>Perfil</TitleText>
+          <ButtonLogout onPress={() => {}}>
+            <Icon name="power" size={24} color="#AEAEB3" />
+          </ButtonLogout>
+        </HeaderWrapper>
+      </Header>
+
+      <InfoProfile>
+        <ProfilePicture>
+          <ImageProfile source={ProfileImage} style={{ borderRadius: 90 }} />
+        </ProfilePicture>
+      </InfoProfile>
     </Container>
   );
 };
--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -1,6 +1,50 @@
+import { Platform, StatusBar, FlatList } from 'react-native';
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+const heightScreen = StatusBar.currentHeight || 0;
+
+export const Container = styled.SafeAreaView.attrs({
+  paddingTop: Platform.OS === 'android' ? heightScreen : 0,
+})`
   flex: 1;
-  justify-content: center;
+  background-color: #ffffff;
+`;
+
+export const Header = styled.View`
+  background-color: #1b1b1f;
+  padding: 32px 25px;
+  height: ${228 - heightScreen}px;
+`;
+
+export const HeaderWrapper = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const TitleText = styled.Text`
+  font-size: 25px;
+  color: #ffffff;
+  font-family: 'Archivo-SemiBold';
+`;
+
+export const ButtonEditProfile = styled.TouchableOpacity``;
+
+export const ButtonLogout = styled.TouchableOpacity``;
+
+export const InfoProfile = styled.View`
+  flex: 1;
+`;
+
+export const ProfilePicture = styled.View`
+  overflow: hidden;
+  width: 180px;
+  height: 180px;
+  margin: auto;
+  margin-top: -90px;
+`;
+
+export const ImageProfile = styled.Image`
+  width: 100%;
+  height: 100%;
 `;
